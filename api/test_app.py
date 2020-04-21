8a3a57c5a5d435f8ed505d149c06747cc2c9f6d2
otalabi <oatalabi@gmail.com>
2020-04-20 17:01:32 -0700

    add test and requirement files

---
+++ api/test_app.py
@@ -0,0 +1,33 @@
+import json
+import unittest
+from app_api import app, getCount
+
+class BasicTestCase(unittest.TestCase):
+    def test_api(self):
+        tester = app.test_client(self)
+        response = tester.get('/api', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json.loads(response.data)), 10)
+
+    def test_method(self):
+        self.assertEqual(getCount([{"joke": "good Good better girl clean machine"},
+                                    {"joke": "better Synaptitude good! clean machine brain"},
+                                    {"joke": "good the technology is better clean good."}], 10),  
+                                    [{'count': 5, 'rank': 1, 'term': 'good'},
+                                        {'count': 3, 'rank': 2, 'term': 'better'},
+                                        {'count': 3, 'rank': 3, 'term': 'clean'},
+                                        {'count': 2, 'rank': 4, 'term': 'machine'},
+                                        {'count': 1, 'rank': 5, 'term': 'brain'},
+                                        {'count': 1, 'rank': 6, 'term': 'girl'},
+                                        {'count': 1, 'rank': 7, 'term': 'is'},
+                                        {'count': 1, 'rank': 8, 'term': 'synaptitude'},
+                                        {'count': 1, 'rank': 9, 'term': 'technology'},
+                                        {'count': 1, 'rank': 10, 'term': 'the'}
+                                    ])
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
